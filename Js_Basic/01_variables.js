4ac615e911828cd501c17e866ef8e05b3e3223b1
Prashant Rai <raiprashant9935@gmail.com>
2024-12-02 16:39:24 +0530

    first

---
+++ Js_Basic/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144533
+let accountemail = "pra@gmail.com"
+var accountPassword = "12345"
+accountCity = "Gkp"
+
+
+//accountId = 2  //const cant be changed
+
+accountemail = "ac@gmail.com"
+accountPassword = "2121223"
+accountCity = "Bihar"
+console.log(accountId);
+
+console.table([accountId , accountemail , accountPassword]);
---
+++ Js_Basic/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use Strict";
+//alert(3 + 3);
+
+
+let name  = "Prashant"
+let age = 18
+let isLoggedIn = false
+
+//==>Primitive datatypes
+//number => 2 to power 53
+//BigInt
+//string =>""
+//boolean => true/false
+//null => standalone vaslue(more in namaste js)
+//undefined =>
+// symbol => to find uniqueness
+
+//object
+
+console.log(typeof "Prashant");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
