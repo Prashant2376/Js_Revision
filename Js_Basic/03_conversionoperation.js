8684f026794d4f53a5006e68f3be20a8a3a7afc1
Prashant Rai <raiprashant9935@gmail.com>
2024-12-03 16:09:12 +0530

    first

---
+++ Js_Basic/03_conversionoperation.js
@@ -0,0 +1,21 @@
+let score = "33abc";
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+//if score is converted to 
+//null 0
+//undefined Nan
+//boolean 0/1
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+let someNumber  = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
